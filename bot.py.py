ed2899b270e7a6ec2230db83f9194b89351f7cb2
fizshopka <kssenfn@gmail.com>
2026-09-02 07:34:59 +0100

    fizshopka

--- bot.py.py
+++ bot.py.py
@@ -14,7 +14,7 @@ from aiogram.types import (
 # НАСТРОЙКИ
 # ==================================================
 
-TOKEN = "8977327110:AAGSUtOhzv2w3c0XDAmqTNu0JFNjpwVCKmc"
+TOKEN = "ВСТАВЬ_НОВЫЙ_ТОКЕН"
 
 # ТВОЙ Telegram ID
 ADMIN_ID = 1335121990
@@ -84,7 +84,7 @@ async def start(message: Message):
 
         await message.answer(
             "⚡ Fizshopka\n\n"
-            "Магазин виртуальных номеров.\n\n"
+            "Магазин физ номеров.\n\n"
             "Нажми кнопку ниже:",
             reply_markup=keyboard
         )
